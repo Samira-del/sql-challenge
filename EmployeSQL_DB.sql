1309928612f5e94759ba92345fb0ba44db3cffdf
Samira-del <sa_mohamadzadeh@yahoo.com>
2020-02-12 22:20:25 -0800

    update tow files

--- EmployeSQL_DB.sql
+++ EmployeSQL_DB.sql
@@ -1,62 +1,3 @@
--- CREATE TABLE departments (
---     dept_no VARCHAR(10)   NOT NULL,
---     dept_name VARCHAR(30)   NOT NULL,
---     CONSTRAINT pk_departments PRIMARY KEY (dept_no)
--- );
-
--- CREATE TABLE dept_emp (
---     emp_no INTEGER   NOT NULL,
---     dept_no VARCHAR(10)   NOT NULL,
---     from_date DATE   NOT NULL,
---     to_date DATE   NOT NULL,
---     CONSTRAINT pk_dept_emp PRIMARY KEY (emp_no, dept_no)
--- );
-
--- CREATE TABLE dept_manager (
---     dept_no VARCHAR(10)   NOT NULL,
--- 	emp_no INTEGER   NOT NULL,
---     from_date DATE   NOT NULL,
---     to_date DATE   NOT NULL
--- );
-
--- CREATE TABLE employees (
---     emp_no INTEGER   NOT NULL,
---     birth_date DATE   NOT NULL,
---     first_name VARCHAR   NOT NULL,
---     last_name VARCHAR   NOT NULL,
---     gender VARCHAR(1)   NOT NULL,
---     hire_date DATE   NOT NULL,
---     CONSTRAINT pk_employees PRIMARY KEY (emp_no)
--- );
-
--- CREATE TABLE salaries (
---     emp_no INTEGER   NOT NULL,
---     salary INTEGER   NOT NULL,
---     from_date DATE   NOT NULL,
---     to_date DATE   NOT NULL,
---     CONSTRAINT pk_salaries PRIMARY KEY (emp_no)
--- );
-
--- CREATE TABLE titles (
---     emp_no INTEGER   NOT NULL,
---     title VARCHAR   NOT NULL,
---     from_date DATE   NOT NULL,
---     to_date DATE   NOT NULL
--- );
-
--- ALTER TABLE dept_emp ADD CONSTRAINT fk_departments_dept_no FOREIGN KEY(dept_no)
--- REFERENCES departments (dept_no);
--- ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
--- REFERENCES titles (emp_no);
--- ALTER TABLE dept_emp ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
--- REFERENCES salaries (emp_no);
--- ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
--- REFERENCES titles (emp_no);
--- ALTER TABLE dept_manager ADD CONSTRAINT fk_employees_emp_no FOREIGN KEY(emp_no)
--- REFERENCES employees (emp_no);
--- ALTER TABLE dept_manager ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
--- REFERENCES salaries (emp_no);
-
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 -- 1. List the following details of each employee: employee number, last name, first name, gender, and salary.
 select e.emp_no, e.last_name, e.first_name,e.gender,s.salary from employees e
@@ -65,7 +6,7 @@ join salaries s ON e.emp_no = s.emp_no;
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 -- 2. List employees who were hired in 1986.
 select e.emp_no, e.last_name, e.first_name,e.hire_date from employees e
-WHERE e.hire_date >= '01/01/1986';
+WHERE e.hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 -- 3. List the manager of each department with the following information: department number, department name, the manager's 
@@ -95,7 +36,7 @@ select dm.emp_no, e.first_name, e.last_name, d.dept_name
 FROM dept_emp dm
 JOIN employees e ON dm.emp_no = e.emp_no
 JOIN departments d ON dm.dept_no = d.dept_no
-WHERE dept_name = 'Sales' OR dept_name = 'Development'
+WHERE dept_name IN ('Sales', 'Development')
 ORDER BY dept_name;
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
